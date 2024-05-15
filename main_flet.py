741ebbf9e304d6f9cfd00b1cab95b181a65dd348
vlad <russia7780030513@gmail.com>
2024-05-15 16:47:35 +0300

    1

--- main_flet.py
+++ main_flet.py
@@ -9,23 +9,32 @@ class Character:
         self.hp = hp
         self.initiative = initiative
         self.active = active
+        self.btn_visible = True
 
 
 def main(page: ft.Page):
+    def on_window_resize(e):
+        current_width = e.control.width
+        update_ui()
+
     page.title = 'DnD sup'
     page.padding = ft.padding.symmetric(horizontal=30)
     page.window_resizable = True
+    page.scroll = ft.ScrollMode.ADAPTIVE
+    page.window_min_width = 600
+    page.on_resize = on_window_resize
+    page.icon
 
     def create_custom_textfield(index, attr_name, value):
         return ft.TextField(
             value=str(value),
             on_change=lambda e, idx=index, attr=attr_name: update_character_value(idx, attr, e.control.value),
-            height=50,
+            height=45,
             fill_color=first_color,
             border_color=first_color,
             border_width=3,
             text_size=25,
-            expand=True,
+            expand=2,
             text_vertical_align=ft.VerticalAlignment.CENTER,
             text_style=ft.TextStyle(height=1),
             border_radius=15,
@@ -34,9 +43,9 @@ def main(page: ft.Page):
 
     def update_character_value(index, attribute, new_value):
         char = characters_list[index]
-        setattr(char, attribute, new_value)  # Универсальный подход для изменения атрибута персонажа
+        setattr(char, attribute, new_value)
 
-    def create_super_custom_textfield(index, attr_name, value):
+    def create_super_custom_textfield(index, attr_name, value, width_bool):
         def minus_click(e):
             value = getattr(characters_list[index], attr_name)
             update_character_value(index, attr_name, value - 1)
@@ -52,18 +61,19 @@ def main(page: ft.Page):
         text_field = ft.TextField(
             value=str(value),
             on_change=lambda e, idx=index, attr=attr_name: update_character_value(idx, attr, e.control.value),
-            expand=True,
+            expand=1,
             width=120,
             fill_color=first_color,
             border_width=0,
-            text_size=30,
+            text_size=25,
             text_vertical_align=-0.7,
+            text_align=ft.TextAlign.START if width_bool else ft.TextAlign.CENTER,
             text_style=ft.TextStyle(height=1),
             filled=False,
         )
 
-        btn_minus = ft.IconButton(icon=ft.icons.REMOVE, on_click=minus_click)
-        btn_plus = ft.IconButton(icon=ft.icons.ADD, on_click=plus_click)
+        btn_minus = ft.IconButton(icon=ft.icons.REMOVE, on_click=minus_click, visible=width_bool)
+        btn_plus = ft.IconButton(icon=ft.icons.ADD, on_click=plus_click, visible=width_bool)
 
         row = ft.Row(
             controls=[text_field, btn_minus, btn_plus],
@@ -72,7 +82,7 @@ def main(page: ft.Page):
         )
 
         return ft.Container(content=row, alignment=ft.alignment.center, expand=True,
-                            border_radius=15, height=50, border=ft.border.all(3, first_color))
+                            border_radius=15, height=45, border=ft.border.all(3, first_color))
 
     def animate_container(color_on_hover, color_on_leave):
         def on_hover(e):
@@ -92,37 +102,53 @@ def main(page: ft.Page):
         elif attribute == "initiative":
             char.initiative = int(new_value)
 
+    def deactivate_character(e, index):
+        char = characters_list[index]
+        char.active = False
+        update_ui()
+
     def update_ui():
         characters_view.controls.clear()
-        names_text = ", ".join([str(char.kd) for char in characters_list])
-        page.add(ft.Text(value=names_text))
+        btn_visible = page.width >= 1000
         for index, char in enumerate(characters_list):
-            name_text = create_custom_textfield(index, "name", char.name)
-            kd_input = create_super_custom_textfield(index, "kd", char.kd)
-            hp_input = create_super_custom_textfield(index, "hp", char.hp)
-            initiative_input = create_super_custom_textfield(index, "initiative", char.initiative)
-            row_ch = ft.Row(
-                controls=[name_text, kd_input, hp_input, initiative_input],
-                alignment=ft.alignment.center, spacing=20
-            )
-            container_ch = ft.Container(content=row_ch, padding=ft.padding.symmetric(horizontal=10),
-                                        bgcolor=first_color, height=60,
-                                        border_radius=20, animate=ft.animation.Animation(duration=100,
-                                                                                         curve=ft.AnimationCurve.EASE_IN_OUT),
-                                        on_hover=animate_container(color_on_hover=first_color_hover,
-                                                                   color_on_leave=first_color))
-            characters_view.controls.append(container_ch)
+            if char.active:
+                name_text = create_custom_textfield(index, "name", char.name)
+                kd_input = create_super_custom_textfield(index, "kd", char.kd, btn_visible)
+                hp_input = create_super_custom_textfield(index, "hp", char.hp, btn_visible)
+                initiative_input = create_super_custom_textfield(index, "initiative", char.initiative, btn_visible)
+                delete_btn = ft.IconButton(icon=ft.icons.DELETE, expand=False, icon_color='red', width=40,
+                                           on_click=lambda e, idx=index: deactivate_character(e, idx))
+                row_ch = ft.Row(
+                    controls=[name_text, kd_input, hp_input, initiative_input, delete_btn],
+                    alignment=ft.alignment.center, spacing=10
+                )
+                container_ch = ft.Container(content=row_ch, padding=ft.padding.symmetric(horizontal=10),
+                                            bgcolor=first_color, height=50,
+                                            border_radius=20, animate=ft.animation.Animation(duration=100,
+                                                                                             curve=ft.AnimationCurve.EASE_IN_OUT),
+                                            on_hover=animate_container(color_on_hover=first_color_hover,
+                                                                       color_on_leave=first_color))
+                characters_view.controls.append(container_ch)
         page.update()
 
     def add_character(e):
-        character = Character(
-            name=name_input.value,
-            kd=randint(10, 18),
-            hp=randint(10, 100),
-            initiative=randint(1, 20),
-            active=True
-        )
-        characters_list.append(character)
+        new_name = name_input.value.strip()
+        existing_character = next(
+            (char for char in characters_list if char.name.lower() == new_name.lower() and not char.active), None)
+        if existing_character:
+            existing_character.active = True
+            existing_character.initiative = 0
+        else:
+            character = Character(
+                name=new_name,
+                kd=15,
+                hp=20,
+                initiative=0,
+                active=True
+            )
+            characters_list.append(character)
+        name_input.value = ""
+        name_input.update()
         update_ui()
 
     page.vertical_alignment = ft.alignment.top_center
@@ -141,16 +167,43 @@ def main(page: ft.Page):
     add_button = ft.FloatingActionButton(icon=ft.icons.ADD,
                                          height=50,
                                          elevation=0,
-                                         bgcolor='#c0392b',
                                          on_click=add_character)
     input_row = ft.Row(controls=[name_input, add_button])
     input_card = ft.Card(content=input_row, elevation=0)
     input_container = ft.Container(content=input_card, padding=ft.padding.only(top=20))
     page.add(input_container)
 
-    names_col = ['Имя', 'КД', 'ХП', 'Инициатива.']
-    text_cols = [ft.Text(value=i, expand=True, text_align=ft.TextAlign.CENTER) for i in names_col]
-    page.add(ft.Container(ft.Row(controls=text_cols)))
+    def sort_ini():
+        def sort_ini_init(e):
+            characters_list.sort(key=lambda char: char.initiative, reverse=True)
+            update_ui()
+
+        return sort_ini_init
+
+    def rand_ini(e):
+        for char in characters_list:
+            char.initiative = randint(1, 20)
+        sort_ini()(e)
+        update_ui()
+
+    def next_turn(e=None):
+        if characters_list:
+            char = characters_list.pop(0)
+            characters_list.append(char)
+            update_ui()
+
+    rand_ini_btn = ft.ElevatedButton(text='Случ.ИНИ', expand=True, bgcolor=first_color, on_click=rand_ini)
+    sort_ini_btn = ft.ElevatedButton(text='Сортировать', expand=True, bgcolor=first_color, on_click=sort_ini())
+    next_turn_btn = ft.ElevatedButton(text='След. Ход', expand=True, bgcolor=first_color, on_click=next_turn)
+    btn_row = ft.Row(controls=[rand_ini_btn, sort_ini_btn, next_turn_btn])
+    page.add(ft.Container(content=btn_row))
+
+    name_title = ft.Text(value="Имя", expand=2, text_align=ft.TextAlign.CENTER)
+    kd_title = ft.Text(value="КД", expand=1, text_align=ft.TextAlign.CENTER)
+    hp_title = ft.Text(value="ХП", expand=1, text_align=ft.TextAlign.CENTER)
+    ini_title = ft.Text(value="ИНИ", expand=1, text_align=ft.TextAlign.CENTER)
+    text_cols = [name_title, kd_title, hp_title, ini_title]
+    page.add(ft.Container(ft.Row(controls=text_cols, spacing=10), padding=ft.padding.only(left=10, right=60)))
 
     characters_view = ft.Column()
     page.add(characters_view)
